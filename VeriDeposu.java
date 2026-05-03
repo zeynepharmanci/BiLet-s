2c2d0ba8624031b8403ea60ed7aa6e42d825237a
ertnidanur <ertnidanur@gmail.com>
2026-05-03 17:06:45 +0300

    Update VeriDeposu.java

--- VeriDeposu.java
+++ VeriDeposu.java
@@ -1,12 +1,8 @@
 package BiLets;
 import java.util.ArrayList;
 public class VeriDeposu {
-    // Sistemdeki tüm etkinlikler burada tutulur
     public static ArrayList<Event> etkinlikListesi = new ArrayList<>();
-    // İstersen sistemdeki tüm kullanıcıları da burada tutabilirsin
     public static ArrayList<Person> kullaniciListesi = new ArrayList<>();
-    // Sistemde kesilmiş tüm biletler
-    // Not: Ticket abstract olsa bile ArrayList<Ticket> yazabilirsin.
     public static ArrayList<Ticket> biletListesi = new ArrayList<>();
     
     public static Person sistemeGirisYap(String email, String sifre) {
