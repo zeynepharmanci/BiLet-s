e54b3f29cab7dd648e131b99fbf59afb2bebbcc0
ertnidanur <ertnidanur@gmail.com>
2026-05-07 16:08:01 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,29 +2,34 @@ package BiLets;
 
 import javax.swing.SwingUtilities;
 
-public class Main {
+public class BiLets {
     public static void main(String[] args) {
-
         verileriYukle();
 
+        // 2. Arayüzü başlat
         SwingUtilities.invokeLater(() -> {
             new GirisEkrani().setVisible(true);
         });
     }
 
     private static void verileriYukle() {
+        DosyaYönetimi dosyaYonetimi = new DosyaYönetimi();
+ 
+        VeriDeposu.kullaniciListesi.clear(); 
+        VeriDeposu.kullaniciListesi.addAll(dosyaYonetimi.kullanicilariYukle());
+        if(VeriDeposu.kullaniciListesi.isEmpty()) {
+            VeriDeposu.kullaniciListesi.add(new Kullanici("Gazi", "Öğrencisi", 20, "gazi@edu.tr", "5550001122", "sifre123"));
+        }
 
-        VeriDeposu.kayitliKullanicilar.add(new Kullanici("Gazi", "Öğrencisi", 20, "gazi@edu.tr", "5550001122", "sifre123"));
+        VeriDeposu.etkinlikListesi = dosyaYonetimi.verileriYukle();
+ 
+        if(VeriDeposu.etkinlikListesi.isEmpty()) {
+            boolean[][] koltuk = new boolean[10][10];
+            VeriDeposu.etkinlikListesi.add(new Sinema("Avatar: Suyun Yolu", "Ankara", 150.0, koltuk, "James Cameron", 190, "Sam Worthington", true, "Bilim Kurgu"));
+            VeriDeposu.etkinlikListesi.add(new Konser("Sertab Erener", "İstanbul", 400.0, koltuk, "Sertab Erener", "Pop"));
+            VeriDeposu.etkinlikListesi.add(new Tiyatro("Bir Delinin Hatıra Defteri", "İzmir", 200.0, koltuk, 1, "Erdal Beşikçioğlu", "Dram"));
 
-        // Örnek Etkinlikler (10x10 koltuk düzeni ile)
-        boolean[][] koltuklar1 = new boolean[10][10];
-        boolean[][] koltuklar2 = new boolean[10][10];
-        boolean[][] koltuklar3 = new boolean[10][10];
-        boolean[][] koltuklar4 = new boolean[10][10];
-
-        VeriDeposu.etkinlikListesi.add(new Sinema("Avatar: Suyun Yolu", "Ankara", 150.0, koltuklar1, "James Cameron", 190, "Sam Worthington", true, "Bilim Kurgu"));
-        VeriDeposu.etkinlikListesi.add(new Konser("Sertab Erener", "İstanbul", 400.0, koltuklar2, "Sertab Erener", "Pop"));
-        VeriDeposu.etkinlikListesi.add(new Tiyatro("Bir Delinin Hatıra Defteri", "İzmir", 200.0, koltuklar3, "1", "Erdal Beşikçioğlu", "Dram"));
-        VeriDeposu.etkinlikListesi.add(new StandUp("Doğu Demirkol", "Ankara", 300.0, koltuklar4, "Doğu Demirkol", 18, "Günlük Hayat"));
+            dosyaYonetimi.verileriKaydet(VeriDeposu.etkinlikListesi);
+        }
     }
 }
