949489fe1ed2c34e5e7e695c29015ccdaad900e8
ertnidanur <ertnidanur@gmail.com>
2026-05-10 17:47:11 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -33,32 +33,39 @@ public class Main {
         }
 
         VeriDeposu.etkinlikListesi.removeIf(e -> e == null);
-if(VeriDeposu.etkinlikListesi.size() < 15) {
+
+        if(VeriDeposu.etkinlikListesi.size() < 15) {
             
             VeriDeposu.etkinlikListesi.clear();
             
             boolean[][] koltuk = new boolean[10][10];
-            VeriDeposu.etkinlikListesi.add(new Sinema("Avatar: Suyun Yolu", "Ankara", 150.0, koltuk, "James Cameron", 190, "Sam Worthington", true, "Bilim Kurgu"));
-            VeriDeposu.etkinlikListesi.add(new Konser("Sertab Erener", "Konya", 400.0, koltuk, "Sertab Erener", "Pop"));
-            VeriDeposu.etkinlikListesi.add(new Tiyatro("Bir Delinin Hatıra Defteri", "Kütahya", 200.0, koltuk, 1, "Erdal Beşikçioğlu", "Dram"));
-            VeriDeposu.etkinlikListesi.add(new Tiyatro("Cimri", "Kütahya", 180.0, koltuk, 2, "Molière", "Komedi"));
-            VeriDeposu.etkinlikListesi.add(new Tiyatro("Profesyonel", "Ankara", 150.0, koltuk, 1, "Dušan Kovačević", "Dram"));
-            VeriDeposu.etkinlikListesi.add(new Sinema("The Matrix", "Konya", 120.0, koltuk, "Wachowski Kardeşler", 136, "Keanu Reeves", false, "Bilim Kurgu"));
-            VeriDeposu.etkinlikListesi.add(new Sinema("Sherlock Holmes", "Ankara", 140.0, koltuk, "Guy Ritchie", 128, "Robert Downey Jr.", false, "Gizem/Aksiyon"));
-            VeriDeposu.etkinlikListesi.add(new Konser("Madrigal Konseri", "Ankara", 250.0, koltuk, "Madrigal", "Alternatif Rock"));
-            VeriDeposu.etkinlikListesi.add(new Konser("Mor ve Ötesi", "Konya", 350.0, koltuk, "Mor ve Ötesi", "Rock"));
-            VeriDeposu.etkinlikListesi.add(new StandUp("CMXXIV", "Kütahya", 600.0, koltuk, "Cem Yılmaz", 18, "Komedi"));
-            VeriDeposu.etkinlikListesi.add(new StandUp("Doğu Demirkol", "Ankara", 250.0, koltuk, "Doğu Demirkol", 15, "Komedi"));
-            VeriDeposu.etkinlikListesi.add(new Tiyatro("Zengin Mutfağı", "Konya", 220.0, koltuk, 2, "Vasıf Öngören", "Dram/Komedi"));
-            VeriDeposu.etkinlikListesi.add(new Tiyatro("Amadeus", "Ankara", 300.0, koltuk, 2, "Peter Shaffer", "Biyografi"));
-            VeriDeposu.etkinlikListesi.add(new Sinema("Dune: Çöl Gezegeni 2", "Kütahya", 200.0, koltuk, "Denis Villeneuve", 166, "Timothée Chalamet", true, "Bilim Kurgu"));
-            VeriDeposu.etkinlikListesi.add(new Sinema("Oppenheimer", "Konya", 180.0, koltuk, "Christopher Nolan", 180, "Cillian Murphy", false, "Biyografi/Dram"));
-            VeriDeposu.etkinlikListesi.add(new Konser("Duman", "Kütahya", 300.0, koltuk, "Duman", "Rock"));
-            VeriDeposu.etkinlikListesi.add(new Konser("Melike Şahin", "Ankara", 280.0, koltuk, "Melike Şahin", "Pop"));
-            VeriDeposu.etkinlikListesi.add(new StandUp("Hasan Can Kaya", "Kütahya", 400.0, koltuk, "Hasan Can Kaya", 18, "Kara Mizah"));
-            VeriDeposu.etkinlikListesi.add(new StandUp("Baturay Özdemir", "Konya", 200.0, koltuk, "Baturay Özdemir", 18, "Komedi"));
+            
+            etkinlikEkle(new Sinema("Avatar: Suyun Yolu", "Ankara", 150.0, koltuk, "James Cameron", 190, "Sam Worthington", true, "Bilim Kurgu"), "15 Mayıs Cuma 20:00");
+            etkinlikEkle(new Konser("Sertab Erener", "Konya", 400.0, koltuk, "Sertab Erener", "Pop"), "16 Mayıs Cumartesi 21:00");
+            etkinlikEkle(new Tiyatro("Bir Delinin Hatıra Defteri", "Kütahya", 200.0, koltuk, 1, "Erdal Beşikçioğlu", "Dram"), "18 Mayıs Pazartesi 19:30");
+            etkinlikEkle(new Tiyatro("Cimri", "Kütahya", 180.0, koltuk, 2, "Molière", "Komedi"), "19 Mayıs Salı 20:00");
+            etkinlikEkle(new Tiyatro("Profesyonel", "Ankara", 150.0, koltuk, 1, "Dušan Kovačević", "Dram"), "20 Mayıs Çarşamba 20:00");
+            etkinlikEkle(new Sinema("The Matrix", "Konya", 120.0, koltuk, "Wachowski Kardeşler", 136, "Keanu Reeves", false, "Bilim Kurgu"), "21 Mayıs Perşembe 18:00");
+            etkinlikEkle(new Sinema("Sherlock Holmes", "Ankara", 140.0, koltuk, "Guy Ritchie", 128, "Robert Downey Jr.", false, "Gizem/Aksiyon"), "22 Mayıs Cuma 19:00");
+            etkinlikEkle(new Konser("Madrigal Konseri", "Ankara", 250.0, koltuk, "Madrigal", "Alternatif Rock"), "23 Mayıs Cumartesi 21:30");
+            etkinlikEkle(new Konser("Mor ve Ötesi", "Konya", 350.0, koltuk, "Mor ve Ötesi", "Rock"), "24 Mayıs Pazar 22:00");
+            etkinlikEkle(new StandUp("CMXXIV", "Kütahya", 600.0, koltuk, "Cem Yılmaz", 18, "Komedi"), "25 Mayıs Pazartesi 21:00");
+            etkinlikEkle(new StandUp("Doğu Demirkol", "Ankara", 250.0, koltuk, "Doğu Demirkol", 15, "Komedi"), "26 Mayıs Salı 20:30");
+            etkinlikEkle(new Tiyatro("Zengin Mutfağı", "Konya", 220.0, koltuk, 2, "Vasıf Öngören", "Dram/Komedi"), "27 Mayıs Çarşamba 19:00");
+            etkinlikEkle(new Tiyatro("Amadeus", "Ankara", 300.0, koltuk, 2, "Peter Shaffer", "Biyografi"), "28 Mayıs Perşembe 20:00");
+            etkinlikEkle(new Sinema("Dune: Çöl Gezegeni 2", "Kütahya", 200.0, koltuk, "Denis Villeneuve", 166, "Timothée Chalamet", true, "Bilim Kurgu"), "29 Mayıs Cuma 19:30");
+            etkinlikEkle(new Sinema("Oppenheimer", "Konya", 180.0, koltuk, "Christopher Nolan", 180, "Cillian Murphy", false, "Biyografi/Dram"), "30 Mayıs Cumartesi 20:00");
+            etkinlikEkle(new Konser("Duman", "Kütahya", 300.0, koltuk, "Duman", "Rock"), "1 Haziran Pazartesi 21:00");
+            etkinlikEkle(new Konser("Melike Şahin", "Ankara", 280.0, koltuk, "Melike Şahin", "Pop"), "2 Haziran Salı 21:30");
+            etkinlikEkle(new StandUp("Hasan Can Kaya", "Kütahya", 400.0, koltuk, "Hasan Can Kaya", 18, "Kara Mizah"), "3 Haziran Çarşamba 20:00");
+            etkinlikEkle(new StandUp("Baturay Özdemir", "Konya", 200.0, koltuk, "Baturay Özdemir", 18, "Komedi"), "4 Haziran Perşembe 20:30");
 
             dosyaYonetimi.verileriKaydet(VeriDeposu.etkinlikListesi);
         }
+    }
+
+    private static void etkinlikEkle(Event etkinlik, String tarih) {
+        etkinlik.setTarih(tarih);
+        VeriDeposu.etkinlikListesi.add(etkinlik);
     }
 }
