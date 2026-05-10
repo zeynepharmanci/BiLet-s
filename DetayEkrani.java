e30b9bfdd36f1c353a1a9c82d75d1d2a38a584af
ertnidanur <ertnidanur@gmail.com>
2026-05-10 17:06:28 +0300

    Update DetayEkrani.java

--- DetayEkrani.java
+++ DetayEkrani.java
@@ -8,16 +8,31 @@ public class DetayEkrani extends JFrame {
 
     public DetayEkrani(Event e, JFrame eskiEkran) {
         setTitle(e.getEventname() + " - Detaylar");
-        setSize(500, 600);
+        setSize(800, 750); 
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
-        setLayout(new BorderLayout(20, 20));
+        setLayout(new BorderLayout());
+        setExtendedState(JFrame.MAXIMIZED_BOTH); 
 
         getContentPane().setBackground(new Color(255, 240, 245));
 
+        JPanel pnlIcerik = new JPanel();
+        pnlIcerik.setLayout(new BoxLayout(pnlIcerik, BoxLayout.Y_AXIS));
+        pnlIcerik.setBackground(new Color(255, 240, 245));
+        pnlIcerik.setBorder(BorderFactory.createEmptyBorder(20, 50, 30, 50));
+
+        JLabel lblBaslik = new JLabel(e.getEventname());
+        lblBaslik.setFont(new Font("Segoe UI", Font.BOLD, 36)); 
+        lblBaslik.setForeground(new Color(255, 0, 127)); 
+        lblBaslik.setAlignmentX(Component.CENTER_ALIGNMENT); 
+        
+        pnlIcerik.add(lblBaslik);
+        pnlIcerik.add(Box.createVerticalStrut(15)); 
+
         JLabel lblResim = new JLabel("", SwingConstants.CENTER); 
-        lblResim.setPreferredSize(new Dimension(400, 250));
-        lblResim.setBorder(BorderFactory.createLineBorder(new Color(255, 0, 127), 2));
+        lblResim.setPreferredSize(new Dimension(600, 400));
+        lblResim.setMaximumSize(new Dimension(600, 400)); 
+        lblResim.setBorder(BorderFactory.createLineBorder(new Color(255, 0, 127), 3));
         lblResim.setOpaque(true);
         lblResim.setBackground(Color.WHITE);
 
@@ -26,7 +41,7 @@ public class DetayEkrani extends JFrame {
             URL resimURL = getClass().getResource("resimler/" + kategoriAdi + ".jpg");
             if (resimURL != null) {
                 ImageIcon orijinalIkon = new ImageIcon(resimURL);
-                Image boyutlandirilmisResim = orijinalIkon.getImage().getScaledInstance(400, 250, Image.SCALE_SMOOTH);
+                Image boyutlandirilmisResim = orijinalIkon.getImage().getScaledInstance(600, 400, Image.SCALE_SMOOTH);
                 lblResim.setIcon(new ImageIcon(boyutlandirilmisResim));
             } else {
                 lblResim.setText(e.getClass().getSimpleName() + " resmi bulunamadı");
@@ -37,21 +52,18 @@ public class DetayEkrani extends JFrame {
 
         JPanel pnlResimUst = new JPanel(new FlowLayout(FlowLayout.CENTER));
         pnlResimUst.setOpaque(false); 
-        pnlResimUst.setBorder(BorderFactory.createEmptyBorder(15, 0, 0, 0));
         pnlResimUst.add(lblResim);
 
-        add(pnlResimUst, BorderLayout.NORTH);
+        pnlIcerik.add(pnlResimUst);
+        pnlIcerik.add(Box.createVerticalStrut(25)); 
 
         JPanel pnlBilgi = new JPanel();
         pnlBilgi.setLayout(new BoxLayout(pnlBilgi, BoxLayout.Y_AXIS));
         pnlBilgi.setOpaque(false);
-        pnlBilgi.setBorder(BorderFactory.createEmptyBorder(10, 30, 20, 30));
+        pnlBilgi.setAlignmentX(Component.CENTER_ALIGNMENT);
+        pnlBilgi.setBorder(BorderFactory.createEmptyBorder(20, 250, 20, 250));
 
-        JLabel lblBaslik = new JLabel(e.getEventname());
-        lblBaslik.setFont(new Font("Segoe UI", Font.BOLD, 26));
-        lblBaslik.setForeground(new Color(255, 0, 127)); 
-
-        String detayMetni = "<html><br><b>Şehir:</b> " + e.getCity() + 
+        String detayMetni = "<html><center><b>Şehir:</b> " + e.getCity() + 
                             "<br><b>Taban Fiyat:</b> " + e.getPrice() + " TL<br><br>";
 
         if (e instanceof Sinema) {
@@ -81,25 +93,33 @@ public class DetayEkrani extends JFrame {
                           "<b>Konu:</b> " + st.getGosteriKonusu() + "<br>" +
                           "<b>Yaş Sınırı:</b> +" + st.getYasSiniri();
         }
-        detayMetni += "</html>";
+        detayMetni += "</center></html>";
 
         JLabel lblDetay = new JLabel(detayMetni);
-        lblDetay.setFont(new Font("Segoe UI", Font.PLAIN, 16));
+        lblDetay.setFont(new Font("Segoe UI", Font.PLAIN, 22)); 
+        lblDetay.setHorizontalAlignment(SwingConstants.CENTER);
+        lblDetay.setAlignmentX(Component.CENTER_ALIGNMENT); 
 
-        pnlBilgi.add(lblBaslik);
         pnlBilgi.add(lblDetay);
-        add(pnlBilgi, BorderLayout.CENTER);
+        pnlIcerik.add(pnlBilgi);
 
-        JPanel pnlButonlar = new JPanel(new FlowLayout(FlowLayout.CENTER, 20, 10));
+        JScrollPane scrollPane = new JScrollPane(pnlIcerik);
+        scrollPane.setBorder(null); 
+        scrollPane.getViewport().setBackground(new Color(255, 240, 245));
+        scrollPane.getVerticalScrollBar().setUnitIncrement(16); 
+
+        add(scrollPane, BorderLayout.CENTER); 
+
+        JPanel pnlButonlar = new JPanel(new FlowLayout(FlowLayout.CENTER, 30, 20));
         pnlButonlar.setOpaque(false); 
-        pnlButonlar.setBorder(BorderFactory.createEmptyBorder(0, 0, 15, 0));
+        pnlButonlar.setBorder(BorderFactory.createEmptyBorder(10, 0, 20, 0));
 
         JButton btnGeri = new JButton("<- Geri Dön");
-        btnGeri.setPreferredSize(new Dimension(130, 40));
+        btnGeri.setPreferredSize(new Dimension(160, 50));
         btnGeri.setBackground(Color.LIGHT_GRAY);
         btnGeri.setForeground(Color.BLACK);
         btnGeri.setFocusPainted(false);
-        btnGeri.setFont(new Font("Segoe UI", Font.BOLD, 14));
+        btnGeri.setFont(new Font("Segoe UI", Font.BOLD, 16));
         
         btnGeri.addActionListener(event -> {
             eskiEkran.setVisible(true); 
@@ -107,11 +127,11 @@ public class DetayEkrani extends JFrame {
         });
 
         JButton btnOnayla = new JButton("Koltuk Seç ve Öde");
-        btnOnayla.setPreferredSize(new Dimension(180, 40));
+        btnOnayla.setPreferredSize(new Dimension(220, 50));
         btnOnayla.setBackground(new Color(255, 0, 127));
         btnOnayla.setForeground(Color.WHITE);
         btnOnayla.setFocusPainted(false);
-        btnOnayla.setFont(new Font("Segoe UI", Font.BOLD, 14));
+        btnOnayla.setFont(new Font("Segoe UI", Font.BOLD, 16));
         
         btnOnayla.addActionListener(event -> {
             new KoltukSecimEkrani(e.getEventname(), e.getPrice()).setVisible(true);
@@ -120,7 +140,8 @@ public class DetayEkrani extends JFrame {
 
         pnlButonlar.add(btnGeri);
         pnlButonlar.add(btnOnayla);
-        add(pnlButonlar, BorderLayout.SOUTH);
+        
+        add(pnlButonlar, BorderLayout.SOUTH); 
 
         setVisible(true);
     }
